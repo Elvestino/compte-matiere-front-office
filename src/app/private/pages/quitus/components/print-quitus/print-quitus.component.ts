8ac3a86cbe23b339e1351d74e3ab61c3e3571f87
TSIANOY Karl Elvestino Dorelin <elvestinodorelin@gmail.com>
2024-04-11 11:51:58 +0300

    refactor : update print quitus

--- src/app/private/pages/quitus/components/print-quitus/print-quitus.component.ts
+++ src/app/private/pages/quitus/components/print-quitus/print-quitus.component.ts
@@ -2,21 +2,44 @@ import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { QuitusService } from '../../../../service/quitus.service';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ServiceService } from '../../../../service/service.service';
 
 @Component({
   selector: 'app-print-quitus',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './print-quitus.component.html',
   styleUrl: './print-quitus.component.scss',
 })
 export class PrintQuitusComponent {
-  constructor(private QuitusS: QuitusService) {
+  constructor(
+    private QuitusS: QuitusService,
+    private service: ServiceService,
+    private formBuilder: FormBuilder
+  ) {
+    this.getService();
     this.getQuitus();
   }
   quitusdata: any[] = [];
+  servicedata: any[] = [];
+  QuitusForm = this.formBuilder.group({
+    numQuitus: [''],
+    dateQuitus: ['', [Validators.required]],
+    nomService: ['', [Validators.required]],
+    observateur: ['', [Validators.required]],
+    ReferenceQuitus: ['', [Validators.required]],
+    objetQuitus: ['', [Validators.required]],
+    montantQuitus: ['', [Validators.required]],
+    exerciceAnnee: ['', [Validators.required]],
+  });
   @ViewChild('content', { static: false }) content: any;
 
+  getService() {
+    this.service.findAll().subscribe((getAll) => {
+      this.servicedata = getAll;
+    });
+  }
   getQuitus() {
     this.QuitusS.findAll().subscribe((getAll) => {
       this.quitusdata = getAll;
@@ -37,5 +60,19 @@ export class PrintQuitusComponent {
       pdf.save('Quitus.pdf');
     });
     this.close.emit();
+  }
+  modifAquitus(item: any) {
+    if (this.QuitusForm) {
+      this.QuitusForm.patchValue({
+        numQuitus: item.numQuitus,
+        dateQuitus: item.dateQuitus,
+        nomService: item.service.nomService,
+        observateur: item.observateur,
+        ReferenceQuitus: item.ReferenceQuitus,
+        montantQuitus: item.montantQuitus,
+        exerciceAnnee: item.exerciceAnnee,
+        objetQuitus: item.objetQuitus,
+      });
+    }
   }
 }
